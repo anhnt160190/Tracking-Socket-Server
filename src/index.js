569d32725821408653f7420e618de3e7328c5830
anhnt <anh.nt160190@gmail.com>
2020-12-16 23:05:27 +0700

    [feat] add template engine

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
+// template engine
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => console.log(`Server is running on ${port}`));
 const io = socketIO(server);
 
 // Execute socket events
 Events(io);
+
+// Router
+app.get('/', function (req, res) {
+  res.render('index');
+});
